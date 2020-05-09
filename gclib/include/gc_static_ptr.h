9e8f1d7d893eb0de2d955aa4fbe93f9abd63a448
Michael Lynch <lyncmi07@yahoo.co.uk>
2020-05-09 16:40:31 +0100

    Revert "Extracted implementation in static_ptr header"
    This reverts commit da129da57984ded1f417df6c332c88ace3149a9b.

--- gclib/include/gc_static_ptr.h
+++ gclib/include/gc_static_ptr.h
@@ -20,73 +20,59 @@ namespace gc
             static_ptr<gc::object>* _back_static_object;
             static_ptr<gc::object>* _fwd_static_object;
 
-            static_ptr();
-            static_ptr(T* object);
-            static_ptr(const gc::static_ptr<T>& sptr2);
+            static_ptr():
+                _object(nullptr)
+            {
+                gc::heap::heap_struct::get()->add_static(this);
+            }
 
-            ~static_ptr();
+            static_ptr(T* object):
+                _object(object)
+            {
+                ensure_gc_object(object);
+                if (object->current_mark() == 'I') gc::heap::heap_struct::get()->remove_from_initialization_list(object);
+                gc::heap::heap_struct::get()->add_static(this);
+            }
 
-            gc::static_ptr<T>& operator=(const gc::static_ptr<T>& sptr2);
-            gc::static_ptr<T>& operator=(const T* object);
+            static_ptr(const gc::static_ptr<T>& sptr2):
+                _object(sptr2._object)
+            {
+                gc::heap::heap_struct::get()->add_static(this);
+            }
 
+            gc::static_ptr<T>& operator=(const gc::static_ptr<T>& sptr2)
+            {
+                _object = sptr2._object;
+                return *this;
+            }
 
-            T* operator->();
+            gc::static_ptr<T>& operator=(const T* object)
+            {
+                _object = object;
+                return *this;
+            }
 
-            T* debug_object();
-            void gc_mark();
+            ~static_ptr()
+            {
+                gc::heap::heap_struct::get()->remove_static(this);
+            }
+
+            T* operator->()
+            {
+                if (_object != nullptr) _object->gc_mark();
+                return _object;
+            }
+
+            T* debug_object()
+            {
+                return _object;
+            }
+
+            void gc_mark()
+            {
+                if (_object != nullptr) _object->gc_mark();
+            }
     };
 }
 
-gc::static_ptr::static_ptr():
-    _object(nullptr)
-{
-    gc::heap::heap_struct::get()->add_static(this);
-}
-
-gc::static_ptr::static_ptr(T* object):
-    _object(object)
-{
-    ensure_gc_object(object);
-    if (object->current_mark() == 'I') gc::heap::heap_struct::get()->remove_from_initialization_list(object);
-    gc::heap::heap_struct::get()->add_static(this);
-}
-
-gc::static_ptr::static_ptr(const gc::static_ptr<T>& sptr2):
-    _object(sptr2._object)
-{
-    gc::heap::heap_struct::get()->add_static(this);
-}
-
-gc::static_ptr<T>& gc::static_ptr::operator=(const gc::static_ptr<T>& sptr2)
-{
-    _object = sptr2._object;
-    return *this;
-}
-
-gc::static_ptr<T>& gc::static_ptr::operator=(const T* object)
-{
-    _object = object;
-    return *this;
-}
-
-gc::static_ptr::~static_ptr()
-{
-    gc::heap::heap_struct::get()->remove_static(this);
-}
-
-T* gc::static_ptr::operator->()
-{
-    if (_object != nullptr) _object->gc_mark();
-    return _object;
-}
-
-T* gc::static_ptr::debug_object()
-{
-    return _object;
-}
-
-void gc::static_ptr::gc_mark()
-{
-    if (_object != nullptr) _object->gc_mark();
-}
 #endif
