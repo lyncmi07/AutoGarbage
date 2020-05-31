fcf297343757727760beac462c9776d19bc8f6c1
Michael Lynch <lyncmi07@yahoo.co.uk>
2020-05-31 20:39:23 +0100

    Adding documentation for static functions

--- gclib/include/gc_statics.h
+++ gclib/include/gc_statics.h
@@ -5,8 +5,21 @@
 
 namespace gc
 {
+    /**
+     * Initiates the AutoGarbage library. This must be called before attempting to allocate any gc::objects.
+     * @param    heap_size                             The size in bytes of the heap.
+     * @param    max_allocation_attempts_before_gc     The maximum number of times an allocation attempt is made for an object before the system gives up and collects garbage.
+     */
     void init(size_t heap_size, unsigned int max_allocation_attempts_before_gc);
+
+    /**
+     * Print verbose debug information to the console about the current state of the AutoGarbage system.
+     */
     void debug();
+
+    /**
+     * Print information to the console about the current state of the AutoGarbage system.
+     */
     void info();
 }
 
